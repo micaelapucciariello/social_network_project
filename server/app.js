7b554bd28c328dfb1329954b7c1e1f5311e51992
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-21 22:44:31 -0300

    added value keys

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = 5000;
+const {MONGOURI} = require('./valuekeys');
+
 
 const app = express()
 
+const customMiddleWare = (req, res, next) =>{
+    console.log("This is a middleware");
+    next() //stops execution
+}
+
 app.get("/", function(req, res){
     res.send("config");
+});
+
+app.get("/home", customMiddleWare, function(req, res){
+    res.send("config home");
 });
 
 app.listen(PORT, ()=>{
---
+++ server/valuekeys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI:"mongodb+srv://mpucciariello:6K35cCT95kzJjt1F@cluster0.w6g6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}

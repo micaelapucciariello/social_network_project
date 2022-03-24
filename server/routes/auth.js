39d14b1767fdb743cf21eb75140e7bf1c2eb4dcf
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-24 16:40:16 -0300

    added user post method

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
-router.get("/",(req,res)=>{
-    res.send("hello world");
-})
-
-router.post("/singup", (req, res)=>{
-    const{name,email,password} = req.body
-    if(!email || !password || !name){
-        res.status(422).json({error: "missing data"});
-    }
-    res.json({message: "data saved"});
+router.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-module.exports = router
+router
+  .post("/signup", (req, res) => {
+    const { name, email, password } = req.body;
+    if (!email || !password || !name) {
+      res.status(422).json({ error: "missing data" });
+    }
+    User.findOne({ email: email }).then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({ error: "email already exists" });
+      }
+
+      const user = new User({
+        email,
+        password,
+        name,
+      });
+
+      user
+        .save()
+        .then((user) => {
+          res.json({ message: "user saved succesfully!" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  })
+
+module.exports = router;

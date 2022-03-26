de8d9659862406b4b5b36cbb5145a8875cb4b6d4
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-26 12:41:12 -0300

    created client structure

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">
+        My Social Network
+      </a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li>
+          <a href="login.html">Login</a>
+        </li>
+        <li>
+          <a href="signup.html">Singup</a>
+        </li>
+        <li>
+          <a href="profile.html">Profile</a>
+        </li>
+      </ul>
+    </div>
+  </nav>;
+};
+
+export default Navbar;

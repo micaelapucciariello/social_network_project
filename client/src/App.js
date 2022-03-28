fdbbef39e8df4e6139140b48ff1675f4f08583a6
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-27 21:05:46 -0300

    fixed error and added routes/screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom'
+import { Home } from './components/screens/Home';
+import { Profile } from './components/screens/Profile';
+import { Signup } from './components/screens/Signup';
+import { Login } from './components/screens/Login';
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar/>
+      <Route exact path= '/'>
+        <Home/>
+      </Route>
+      <Route path= '/signin'>
+        <Login/>
+      </Route>
+      <Route path= '/profile'>
+        <Profile/>
+      </Route>
+      <Route path= '/login'>
+        <Login/>
+      </Route>
+      <Route path= '/signup'>
+        <Signup/>
+      </Route>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,26 @@
 import React from "react";
 
 const Navbar = () => {
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo center">
-        My Social Network
-      </a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li>
-          <a href="login.html">Login</a>
-        </li>
-        <li>
-          <a href="signup.html">Singup</a>
-        </li>
-        <li>
-          <a href="profile.html">Profile</a>
-        </li>
-      </ul>
-    </div>
-  </nav>;
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo b">
+          Social Network Project
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="login.html">Login</a>
+          </li>
+          <li>
+            <a href="signup.html">Signup</a>
+          </li>
+          <li>
+            <a href="profile.html">Profile</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
